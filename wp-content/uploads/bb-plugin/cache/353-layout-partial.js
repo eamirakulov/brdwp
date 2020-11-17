c2f7948e04c65c65d576d8eb50a55978b4095ae1
eamirakulov <emil@amirakulov.com>
2020-11-17 18:17:21 +0600

    sdfsdfsdf

--- wp-content/uploads/bb-plugin/cache/353-layout-partial.js
+++
@@ -1,2 +0,0 @@
-
-(function($){$(function(){clickColumn();});function clickColumn(){$('.click-col').css('cursor','pointer');$('.click-col').on('click',function(event){$(this).find('a')[0].click();});$('.click-col a').on('click',function(event){event.stopPropagation();});}})(jQuery);
--- wp-content/uploads/bb-plugin/cache/7-layout-partial.js
+++
@@ -1,4 +0,0 @@
-
-jQuery(function($){$('#demo-lightbox').click(function(e){e.preventDefault();$('.custom-lightbox').fadeIn();})
-$('.close-lightbox').click(function(e){e.preventDefault();$('.custom-lightbox').fadeOut();})
-jQuery(function($){$('.posts-slider').slick({dots:false,arrows:true,slidesToShow:3,responsive:[{breakpoint:1024,settings:{slidesToShow:2,slidesToScroll:2}},{breakpoint:600,settings:{slidesToShow:1,slidesToScroll:1}},{breakpoint:480,settings:{slidesToShow:1,slidesToScroll:1}}]})});});
