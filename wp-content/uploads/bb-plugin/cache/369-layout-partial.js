45e10b7004bbe6560d263ed28a31d5e21406fea8
eamirakulov <emil@amirakulov.com>
2020-12-01 19:03:03 +0600

    Update 1dec

--- wp-content/uploads/bb-plugin/cache/369-layout-partial.js
+++
@@ -1,2 +0,0 @@
-
-jQuery(function($){$('.posts-slider').slick({dots:false,arrows:true,slidesToShow:3,responsive:[{breakpoint:767,settings:{slidesToShow:2,slidesToScroll:2}},{breakpoint:480,settings:{slidesToShow:1,slidesToScroll:1}}]})});
